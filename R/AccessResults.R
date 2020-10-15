6610ca19b0c546988b2f7e5fbc05b7c0d2bdb959
dpmerrell <davidpmerrell@gmail.com>
2020-10-15 11:02:28 -0500

    debugged the accessor functions (still getting the hang of R packages)

--- R/AccessResults.R
+++ R/AccessResults.R
@@ -1,14 +1,15 @@
-library("RSQLite")
 
 connect_to_results <- function(sqlite_filename){
-  conn = dbConnect(SQLite(), sqlite_filename, flags=SQLITE_RO);
-  conn;
+  conn = RSQLite::dbConnect(RSQLite::SQLite(), sqlite_filename, flags=RSQLite::SQLITE_RO);
+  return(conn);
 }
 
 
 fetch_result <- function(db_conn, a0, a1, b0, b1){
   sql_str = paste("SELECT * FROM Results WHERE (A0, A1, B0, B1) = (",
-                  a0, a1, b0, b1, ")");
-  result = dbGetQuery(conn, sql_str);
-  result;
+                  a0, ",", a1, ",", b0, ",", b1, ")");
+  print("SQL STRING:")
+  print(sql_str)
+  result = DBI::dbGetQuery(conn, sql_str);
+  return(result);
 }
